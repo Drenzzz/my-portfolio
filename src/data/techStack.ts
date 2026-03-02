ffbcc9ea1429cf98cd5317fd9ca2289b27585c62
Drenzzz <realdrenzzz@gmail.com>
2026-03-02 12:00:00 +0700

    chore: Update the tech stack

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -2,18 +2,14 @@ import type { TechItem } from "@/types";
 
 export const techStack: TechItem[] = [
   { id: "go", name: "Go" },
-  { id: "kotlin", name: "Kotlin" },
   { id: "typescript", name: "TypeScript" },
   { id: "javascript", name: "JavaScript" },
-  { id: "python", name: "Python" },
   { id: "react", name: "React" },
   { id: "nextdotjs", name: "Next.js" },
   { id: "astro", name: "Astro" },
   { id: "tailwindcss", name: "Tailwind CSS" },
   { id: "shadcnui", name: "shadcn/ui" },
-  { id: "flutter", name: "Flutter" },
   { id: "hono", name: "Hono.js" },
-  { id: "mongodb", name: "MongoDB" },
   { id: "postgresql", name: "PostgreSQL" },
   { id: "git", name: "Git" },
   { id: "linux", name: "Linux" },
